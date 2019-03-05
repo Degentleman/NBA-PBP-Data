ec4b5e6a07c5044076d668b38960b98109568c9c
Degentleman <42943580+Degentleman@users.noreply.github.com>
2019-03-05 17:50:56 -0500

    Updated NSAmodel.py
    Edited script.

--- NSAmodel.py
+++ NSAmodel.py
@@ -13,7 +13,7 @@ import statsmodels.api as sm
 import TeamNSAmodel as teamNSA
 
 
-NBA_Legend = pd.read_csv('/Users/devinpower-bearden/AnacondaProjects/NBA/Synergy Project Files/NBA Player DF - 2019.csv', delimiter = ',')   
+NBA_Legend = pd.read_csv('NBA Player DF - 2019.csv', delimiter = ',')   
 
 # Common path functions:
 path = nx.dijkstra_path
