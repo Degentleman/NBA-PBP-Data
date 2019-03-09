860988fd414a8ebfe0f2fbff71a850924d76176b
Degentleman <42943580+Degentleman@users.noreply.github.com>
2019-03-08 22:50:11 -0500

    Updated filename and script.

--- NBAThreePointers.py
+++ NBAThreePointers.py
@@ -16,27 +16,20 @@ NBA_Legend = pd.read_csv('NBA Player DF - 2019.csv', delimiter = ',')
 Team_Legend = pd.read_csv('NBA PBP - Team Legend.csv', delimiter = ',')
 
 #Today's Schedule
-schedule = [['NYK','SAC'],
-            ['ATL', 'BKN'],
-            ['MIN', 'WAS'],
-            ['LAL', 'BOS'],
-            ['MIL', 'CHA'],
-            ['POR', 'PHX'],
-            ['CHI', 'DET'],
-            ['GSW', 'DEN'],
+schedule = [['GSW', 'DEN'],
             ['LAC', 'OKC']]
 
 game_ids_list = []
 columns = ['Player One', 'TeamID', 'locX', 'locY', 'mtype', 'etype']
 threes_df = pd.DataFrame(columns=columns)
-for matchup in schedule[-2:]:
+for matchup in schedule:
     home_key = matchup[0]
     away_key = matchup[1]
     home_game_ids, home_id = BoxScores.GetIDs(home_key)
     away_game_ids, away_id = BoxScores.GetIDs(away_key)
     home_team_name, home_team, home_team_df, home_coach_df, home_df = GetDB(home_id)
     away_team_name, away_team, away_team_df, away_coach_df, away_df = GetDB(away_id)
-    game_ids = list(np.unique(home_game_ids[1:6]+away_game_ids[1:6]))
+    game_ids = list(np.unique(home_game_ids[0:10]+away_game_ids[0:10]))
     for game_id in sorted(game_ids):
         pbp_df, pbpsumdf, filename, home_team, away_team = Reader(game_id, season)
         entry = pbp_df[(pbp_df.opt1 == '3')][columns]
