cc600825ab0bbf14e14acb72efd8ff26761428cf
Degentleman <42943580+Degentleman@users.noreply.github.com>
2019-10-23 16:58:02 -0700

    Updated player DB python script
    Updated NBA season to 2019-20 in file.

--- NBA_Player_DB.py
+++ NBA_Player_DB.py
@@ -23,7 +23,7 @@ for team_id in NBA_Team_IDs:
     mapping = {team_id:team_code}
     
     # Scrape URL for Team
-    url = 'https://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=2018-19&TeamID={team_id}'.format(team_id=team_id)
+    url = 'https://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=2019-20&TeamID={team_id}'.format(team_id=team_id)
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
     response = requests.get(url, headers = headers)
     
